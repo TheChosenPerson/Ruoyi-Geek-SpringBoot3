da9c38f47699b1bf55fa4e647fa63a2d9e73ed6f
D <3066417822@qq.com>
2024-02-23 22:04:06 +0800

    update sql

--- sql/online.sql
+++ sql/online.sql
@@ -16,7 +16,7 @@ CREATE TABLE online_mb (
 
 -- 菜单 SQL
 insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
-values('mybatis在线接口', '3', '1', 'mb', 'tool/online/mb/index', 1, 0, 'C', '0', '0', 'online:mb:list', 'code', 'admin', sysdate(), '', null, 'mybatis在线接口菜单');
+values('mybatis在线接口', '3', '1', 'mb', 'online/mb/index', 1, 0, 'C', '0', '0', 'online:mb:list', 'code', 'admin', sysdate(), '', null, 'mybatis在线接口菜单');
 
 -- 按钮父菜单ID
 SELECT @parentId := LAST_INSERT_ID();
